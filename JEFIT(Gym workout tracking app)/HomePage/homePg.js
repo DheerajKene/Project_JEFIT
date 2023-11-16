8c882500341584e7efafaaecfef6274ad2c90880
Dhiraj Kene <kdprakash1712@gmail.com>
2023-11-16 22:49:18 +0530

    Project of JEFIT is submitted

---
+++ JEFIT(Gym workout tracking app)/HomePage/homePg.js
@@ -0,0 +1,15 @@
+
+var currentSlide = 0;
+  var slider = document.getElementById("slider");
+  var slides = document.querySelectorAll(".slider-item");
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateSlider();
+  }
+  function updateSlider() {
+    slider.style.transform = "translateX(" + -currentSlide * 100 + "%)";
+  }
+  setInterval(nextSlide, 2000);
+
+ 
+  
